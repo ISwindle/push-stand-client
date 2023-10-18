b70635e67ce6b2f21700b8b4c4ed30449fae75aa
Isaac Swindle <109982554+TheSwinDiesel@users.noreply.github.com>
2023-10-17 19:03:03 -0500

    Initial Commit

---
+++ Push Stand/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Push Stand
+//
+//  Created by Isaac Swindle on 10/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
