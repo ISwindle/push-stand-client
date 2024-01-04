8dac9eed15b1b4fdd551eb23360d91e34d61c9e4
Tony Russell <tonyrussell@Tonys-MacBook-Air.local>
2024-01-04 10:50:25 -0500

    circular progress bar dashed, still need to make it animated and possibly reving

--- Push Stand/Views/CircularProgressBar.swift
+++ Push Stand/Views/CircularProgressBar.swift
@@ -4,6 +4,7 @@ class CircularProgressBar: UIView {
 
     private var progressLayer = CAShapeLayer()
     private var trackLayer = CAShapeLayer()
+    private var blurLayer = CAShapeLayer()
 
     var progress: CGFloat = 0.25 {
         didSet {
@@ -23,25 +24,37 @@ class CircularProgressBar: UIView {
 
     private func createCircularPath() {
         self.backgroundColor = .clear
-        let circlePath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0), radius: (frame.size.width - 1.5) / 2, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0), radius: (frame.size.width - 1.5) / 2, startAngle: -CGFloat.pi / 2, endAngle: 1.5 * CGFloat.pi, clockwise: true)
         
         // Track layer
         trackLayer.path = circlePath.cgPath
         trackLayer.fillColor = UIColor.clear.cgColor
         trackLayer.strokeColor = UIColor.darkGray.cgColor
-        trackLayer.lineWidth = 18.0
-        //trackLayer.lineDashPattern = [3,4.5]
+        trackLayer.lineWidth = 22.0
+        trackLayer.lineDashPattern = [3,4.5] //100 lines
         trackLayer.strokeEnd = 1.0
         layer.addSublayer(trackLayer)
 
+        // Blurred layer - would be cool to have it rev to 100 then go back down to progress spot
+        blurLayer.path = circlePath.cgPath
+        blurLayer.fillColor = UIColor.clear.cgColor
+        blurLayer.strokeColor = UIColor.blue.cgColor
+        blurLayer.lineWidth = 22.0
+        blurLayer.lineDashPattern = [3,4.5] //100 (blurred) lines behind progress layer
+        blurLayer.strokeEnd = progress
+        layer.addSublayer(blurLayer)
+        //.blur I can't make work here, keep trying other ways.  Maybe you know the secret?
+        
         // Progress layer
         progressLayer.path = circlePath.cgPath
         progressLayer.fillColor = UIColor.clear.cgColor
         progressLayer.strokeColor = UIColor.blue.cgColor
-        progressLayer.lineWidth = 18.0
-        //progressLayer.lineDashPattern = [3,4.5]
+        progressLayer.lineWidth = 22.0
+        progressLayer.lineDashPattern = [3,4.5] //100 lines
         progressLayer.strokeEnd = progress
         layer.addSublayer(progressLayer)
+        
+        
     }
 
 }
