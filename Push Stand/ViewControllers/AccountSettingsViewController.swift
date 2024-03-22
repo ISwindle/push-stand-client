ee477fb98a48a97f90539fc3245406c3ac481d86
Tony Russell <tonyrussell@Tonys-MacBook-Air.local>
2024-03-22 17:50:47 -0400

    god bless america

--- Push Stand/ViewControllers/AccountSettingsViewController.swift
+++ Push Stand/ViewControllers/AccountSettingsViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class AccountSettingsViewController: UIViewController {
 
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    @IBAction func logoutAction(_ sender: Any) {
+        UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let loginNavController = storyboard.instantiateViewController(identifier: "InitialViewController")
+        
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(loginNavController)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
