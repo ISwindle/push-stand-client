07adec968455ae4c109f7d42a784a27dae59938a
Isaac Swindle <109982554+TheSwinDiesel@users.noreply.github.com>
2024-05-13 21:03:04 -0500

    Email Validation

--- Push Stand/ViewControllers/AccountEmailViewController.swift
+++ Push Stand/ViewControllers/AccountEmailViewController.swift
@@ -1,142 +1,130 @@
-//
-//  AccountEmailViewController.swift
-//  Push Stand
-//
-//  Created by Isaac Swindle on 4/9/24.
-//
-
 import UIKit
 
 class AccountEmailViewController: UIViewController {
-
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var confirmationEmailButton: UIButton!
-    
-    
+
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
     }
+
     @IBAction func updateEmail(_ sender: Any) {
         guard let email = emailText.text, isValidEmail(email) else {
-                print("Invalid email address")
-                self.showAlert(title: "Invalid Email", message: "The entered email is invalid.  Please try again.")
-                // Handle invalid email (e.g., show an alert or error message)
-                return
+            showAlert(title: "Invalid Email", message: "The entered email is invalid. Please try again.")
+            return
         }
         
+        checkEmailAvailability(email) { [weak self] available in
+            guard available else {
+                self?.showAlert(title: "Email Unavailable", message: "The entered email is already in use. Please try a different one.")
+                return
+            }
+            
+            // Proceed with updating the email since it's available
+            self?.updateUserEmail(email)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        confirmationEmailButton.isEnabled = false
+        emailText.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+    }
+
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        confirmationEmailButton.isEnabled = isValidEmail(textField.text ?? "")
+    }
+
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alertController, animated: true, completion: nil)
+    }
+
+    private func checkEmailAvailability(_ email: String, completion: @escaping (Bool) -> Void) {
+        guard let url = URL(string: "https://d516i8vkme.execute-api.us-east-1.amazonaws.com/develop/users/checkEmail") else {
+            print("Invalid URL")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let payload = ["email": email]
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: payload, options: [])
+        } catch {
+            print("Error encoding JSON: \(error)")
+            return
+        }
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil,
+                  let httpResponse = response as? HTTPURLResponse,
+                  httpResponse.statusCode == 200 else {
+                print("Error during the network request or invalid status code")
+                return completion(false)
+            }
+
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                   let emailExists = json["email_exists"] as? Bool {
+                    DispatchQueue.main.async {
+                        completion(!emailExists)
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        completion(false)
+                    }
+                }
+            } catch {
+                print("Error parsing the JSON response: \(error)")
+                DispatchQueue.main.async {
+                    completion(false)
+                }
+            }
+        }.resume()
+    }
+
+    private func updateUserEmail(_ email: String) {
+        // Assume currentUser and other dependencies are correctly configured and available
         let url = URL(string: "https://d516i8vkme.execute-api.us-east-1.amazonaws.com/develop/users")!
         var request = URLRequest(url: url)
         request.httpMethod = "PUT"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        // Add authorization header if needed
-        // request.addValue("Bearer \(yourAuthToken)", forHTTPHeaderField: "Authorization")
-        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-            let currentUser = appDelegate.currentUser
-            
-            let payload: [String: Any] = [
-                "UserId": currentUser.uid,
-                "Birthdate": currentUser.birthdate,
-                "Email": emailText.text,
-                "PhoneNumber": currentUser.phoneNumber,
-                "ReminderTime": currentUser.reminderTime,
-                "FirebaseAuthToken": currentUser.firebaseAuthToken,
-            ]
-            
-            do {
-                let jsonData = try JSONSerialization.data(withJSONObject: payload, options: [])
-                request.httpBody = jsonData
-            } catch {
-                print("Error encoding JSON: \(error)")
+
+        let payload: [String: Any] = [
+            "UserId": CurrentUser.shared.uid,
+            "Birthdate": CurrentUser.shared.birthdate,
+            "Email": email,
+            "PhoneNumber": CurrentUser.shared.phoneNumber,
+            "ReminderTime": CurrentUser.shared.reminderTime,
+            "FirebaseAuthToken": CurrentUser.shared.firebaseAuthToken,
+        ]
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: payload, options: [])
+        } catch {
+            print("Error encoding JSON: \(error)")
+            return
+        }
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error making PUT request: \(error)")
                 return
             }
-            
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                if let error = error {
-                    print("Error making PUT request: \(error)")
-                    return
-                }
-                guard let data = data, let response = response as? HTTPURLResponse,
-                      (200...299).contains(response.statusCode) else {
-                    print("Server error or invalid response")
-                    return
-                }
-                
-                if let responseString = String(data: data, encoding: .utf8) {
-                    print("Response from the server: \(responseString)")
-                }
+            guard let data = data, let response = response as? HTTPURLResponse,
+                  (200...299).contains(response.statusCode) else {
+                print("Server error or invalid response")
+                return
             }
-            
-            task.resume()
-            // Create the alert controller
-            let alert = UIAlertController(title: "Update!", message: "Email updated successfully", preferredStyle: .alert)
-            // Add OK action
-            let okAction = UIAlertAction(title: "OK", style: .default, handler: { _ in
-                if let navigationController = self.navigationController {
-                    // If AccountSettingsViewController is two view controllers back in the navigation stack
-                    if let accountSettingsViewController = navigationController.viewControllers.first(where: { $0 is AccountSettingsViewController }) {
-                        navigationController.popToViewController(accountSettingsViewController, animated: true)
-                    }
-                }
-            })
-            alert.addAction(okAction)
-            
-            // Present the alert to the user
-            self.present(alert, animated: true, completion: nil)
-        
-            
-            // Use DispatchQueue to dismiss the alert after a delay
-            //DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { // Change 2.0 to your desired number of seconds
-              //  alert.dismiss(animated: true, completion: nil)
-            //}
-        }
-        
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Hide the submit button initially
-        confirmationEmailButton.isEnabled = false
-                
-        // Add observer for text field editing changes
-        emailText.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
 
-        // Do any additional setup after loading the view.
-    }
-    
-    @objc func textFieldDidChange(_ textField: UITextField) {
-        
-        // Assuming nextButton is accessible here, and phoneNumberTextField is your UITextField
-            if let text = textField.text, isValidEmail(text) {
-                confirmationEmailButton.isEnabled = true
-            } else {
-                confirmationEmailButton.isEnabled = false
+            DispatchQueue.main.async {
+                self.showAlert(title: "Success", message: "Email updated successfully")
             }
-        
+        }.resume()
     }
-    
-    func showAlert(title: String, message: String, dismissViewController: Bool = false) {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-            if dismissViewController {
-                if let navigationController = self.navigationController {
-                    // If AccountSettingsViewController is two view controllers back in the navigation stack
-                    if let accountSettingsViewController = navigationController.viewControllers.first(where: { $0 is AccountSettingsViewController }) {
-                        navigationController.popToViewController(accountSettingsViewController, animated: true)
-                    }
-                }
-            }
-        }
-        alertController.addAction(okAction)
-        present(alertController, animated: true, completion: nil)
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Push Stand/ViewControllers/SignUpUsernamePasswordViewController.swift
+++ Push Stand/ViewControllers/SignUpUsernamePasswordViewController.swift
@@ -8,51 +8,106 @@ class SignUpUsernamePasswordViewController: UIViewController {
     
     var dataManager = OnboardingManager.shared
     
-    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
-        view.endEditing(true)
-    }
     @IBAction func enterUnAndPw(_ sender: Any) {
     }
     
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         nextButton.isEnabled = false
+        usernameTextField.addTarget(self, action: #selector(usernameFieldDidChange(_:)), for: .editingChanged)
         passwordTextField.addTarget(self, action: #selector(passwordFieldDidChange(_:)), for: .editingChanged)
-
+    }
+    
+    @objc func usernameFieldDidChange(_ textField: UITextField) {
+        validateForm()
     }
     
     @objc func passwordFieldDidChange(_ textField: UITextField) {
-        if let username = usernameTextField.text, isValidEmail(username), let password = passwordTextField.text, isValidPassword(password) {
-                // Handle valid username, e.g., enable a button or change a label color
-                nextButton.isEnabled = true
-            } else {
-                // Handle invalid username
-                nextButton.isEnabled = false
-            }
+        validateForm()
+    }
+    
+    func validateForm() {
+        if let username = usernameTextField.text, isValidEmail(username),
+           let password = passwordTextField.text, isValidPassword(password) {
+            nextButton.isEnabled = true
+        } else {
+            nextButton.isEnabled = false
+        }
     }
     
     func isValidPassword(_ password: String) -> Bool {
-        return password.count > 7
+        return password.count >= 6
     }
     
     @IBAction func next(_ sender: Any) {
-        
-        guard let username = usernameTextField.text, username.count > 3 else {
-            presentAlertWithTitle(title: "Invalid Username", message: "Username must be at least 3 characters long.", options: "OK") { (option) in
-                print("Option selected: \(option)")
-            }
+        guard let email = usernameTextField.text, isValidEmail(email) else {
+            presentAlertWithTitle(title: "Invalid Email", message: "Please enter a valid email address.", options: "OK") { _ in }
             return
         }
         
-        guard let password = passwordTextField.text, password.count >= 6 else {
-            presentAlertWithTitle(title: "Invalid Password", message: "Password must be at least 6 characters long.", options: "OK") { (option) in
-                print("Option selected: \(option)")
+        checkEmailAvailability(email) { [weak self] available in
+            guard available else {
+                self?.presentAlertWithTitle(title: "Email Unavailable", message: "This email is already in use. Please try another one.", options: "OK") { _ in }
+                return
             }
+            
+            // If email is available and valid, proceed with next steps
+            self?.proceedToNextScreen()
+        }
+    }
+    
+    private func checkEmailAvailability(_ email: String, completion: @escaping (Bool) -> Void) {
+        let urlString = "https://d516i8vkme.execute-api.us-east-1.amazonaws.com/develop/users/checkEmail"
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
             return
         }
         
-        // If the password meets the requirement, proceed with saving data and performing segue
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let payload = ["email": email]
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: payload, options: [])
+        } catch {
+            print("Error encoding JSON: \(error)")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil,
+                  let httpResponse = response as? HTTPURLResponse,
+                  httpResponse.statusCode == 200 else {
+                print("Network request failed or bad status code")
+                return completion(false)
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                   let emailExists = json["email_exists"] as? Bool {
+                    DispatchQueue.main.async {
+                        completion(!emailExists)
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        completion(false)
+                    }
+                }
+            } catch {
+                print("Error parsing JSON: \(error)")
+                DispatchQueue.main.async {
+                    completion(false)
+                }
+            }
+        }.resume()
+    }
+    
+    private func proceedToNextScreen() {
         dataManager.onboardingData.username = usernameTextField.text
         dataManager.onboardingData.email = usernameTextField.text
         dataManager.onboardingData.password = passwordTextField.text
@@ -64,11 +119,10 @@ class SignUpUsernamePasswordViewController: UIViewController {
     func presentAlertWithTitle(title: String, message: String, options: String..., completion: @escaping (Int) -> Void) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         for (index, option) in options.enumerated() {
-            alertController.addAction(UIAlertAction.init(title: option, style: .default, handler: { (action) in
+            alertController.addAction(UIAlertAction(title: option, style: .default) { _ in
                 completion(index)
-            }))
+            })
         }
         self.present(alertController, animated: true, completion: nil)
     }
-    
 }
