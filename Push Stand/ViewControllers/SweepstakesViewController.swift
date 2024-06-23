d9ac5eea54490fb9e7456724308cb6f2313c5d38
TRuss10 <tony.russell2012@gmail.com>
2024-06-23 19:34:35 -0400

    pretty much just sweepstakes

--- Push Stand/ViewControllers/SweepstakesViewController.swift
+++ Push Stand/ViewControllers/SweepstakesViewController.swift
@@ -11,40 +11,28 @@ class SweepstakesViewController: UIViewController {
     
     @IBOutlet weak var blurView: UIVisualEffectView!
     @IBOutlet weak var unlockNow: UIButton!
-    @IBOutlet weak var gotIt: UIButton!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var backgroundView: UIImageView!
     @IBOutlet weak var exitView: UIImageView!
     
     override func viewDidLoad() {
-            super.viewDidLoad()
+        super.viewDidLoad()
         
         // Initially hide the blurView
         blurView.isHidden = true
-        
         blurView.layer.cornerRadius = 20
         blurView.layer.masksToBounds = true
 
         // Add target-action for the unlockNow button
         unlockNow.addTarget(self, action: #selector(unlockNowTapped), for: .touchUpInside)
         
-        // Add target-action for the gotIt button
-        gotIt.addTarget(self, action: #selector(gotItTapped), for: .touchUpInside)
-        
-        // Add tap gesture recognizers to exitView and backgroundView
-        let exitTapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
-        exitView.addGestureRecognizer(exitTapGesture)
-        exitView.isUserInteractionEnabled = true
-                
-        let backgroundTapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
-        backgroundView.addGestureRecognizer(backgroundTapGesture)
-        backgroundView.isUserInteractionEnabled = true
-        
+        // Add tap gesture recognizer to the main view (self.view)
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+        self.view.addGestureRecognizer(tapGesture)
     }
             
     // Action method for unlockNow button
     @objc func unlockNowTapped() {
-                
         // Unhide the blurView when the button is tapped
         blurView.isHidden = false
         
@@ -55,19 +43,15 @@ class SweepstakesViewController: UIViewController {
         scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: false)
     }
     
-    // Action method for gotIt button
-    @objc func gotItTapped() {
-        // Hide the blurView when the button is tapped
-        blurView.isHidden = true
-        
-        // Show the unlockNow button
-        unlockNow.isHidden = false
-    }
-    
     // Action method for tap gesture recognizer
     @objc func viewTapped() {
-        // Call gotItTapped method when exitView or backgroundView is tapped
-        gotItTapped()
+        // Check if blurView is visible
+        if !blurView.isHidden {
+            // Hide the blurView when tapped outside of it
+            blurView.isHidden = true
+            
+            // Show the unlockNow button
+            unlockNow.isHidden = false
+        }
     }
 }
-    
